d74208c0bb86bedb47e04aa081f58cfd1d07a524
Super-Louis <liuchao12119@126.com>
2018-05-10 23:33:10 +0800

    add radix_sort

--- sort/radix_sort.py
+++ sort/radix_sort.py
@@ -1,15 +1,21 @@
 def radix_sort(input_list):
+    """
+    基数排序，先按个位进行排序，再按十位排序...
+    时间复杂度O(digit*N)
+    :param input_list:
+    :return:
+    """
     has_digit = True
     count = 0
-    while has_digit:
+    while has_digit: # 直到没有位数则跳出循环
         has_digit = False
-        sorted_list = [list() for _ in range(10)]
+        sorted_list = [list() for _ in range(10)] # 空的列表，用于存储0~9对应的数字
         for i in range(len(input_list)):
-            digit = (input_list[i] // (10**count)) % 10
+            digit = (input_list[i] // (10**count)) % 10 # 得到某一位的数值
             if digit:
                 has_digit = True
             sorted_list[digit].append(input_list[i])
-        input_list = new_list(sorted_list)
+        input_list = new_list(sorted_list) # 得到按照某一位排序后的新列表
         count += 1
     return input_list
 
@@ -22,4 +28,4 @@ def new_list(input_list):
     return new_list
 
 if __name__ == '__main__':
-    print(radix_sort([1,20,33,11,0,16,13,333,222]))
+    print(radix_sort([1,20,11,5,333,0,232,2333333,645,555,789,4444]))
